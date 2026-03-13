ef43669b08757b92296c6d0253ddc6b50b6734a1
Sashin Agrawal <sashin_agrawal@epam.com>
2026-03-13 20:32:41 +0530

    Day3: Problem solving

--- 209.minimum-size-subarray-sum.java
+++ 209.minimum-size-subarray-sum.java
@@ -58,8 +58,19 @@
 // @lc code=start
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        
+        int left = 0;
+        int maxSumSoFar = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            maxSumSoFar += nums[right];
+            while (maxSumSoFar >= target) {
+                res = Math.min(res, right - left + 1);
+                maxSumSoFar -= nums[left++];
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+
     }
 }
 // @lc code=end
-

19c5d5ece5f261490c388b0cd53f0787d8434a3f
Sashin Agrawal <sashin_agrawal@epam.com>
2026-03-20 23:14:25 +0530

    Day 6 Problem on Hashing

---
+++ 128.longest-consecutive-sequence.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=128 lang=java
+ *
+ * [128] Longest Consecutive Sequence
+ *
+ * https://leetcode.com/problems/longest-consecutive-sequence/description/
+ *
+ * algorithms
+ * Medium (47.02%)
+ * Likes:    22814
+ * Dislikes: 1231
+ * Total Accepted:    3.2M
+ * Total Submissions: 6.8M
+ * Testcase Example:  '[100,4,200,1,3,2]'
+ *
+ * Given an unsorted array of integers nums, return the length of the longest
+ * consecutive elements sequence.
+ * 
+ * You must write an algorithm that runs in O(n) time.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+ * Therefore its length is 4.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,0,1,2]
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
